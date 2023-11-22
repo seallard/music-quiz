846702db1c7be0291da96932f33c981aef00e2d3
seallard <sebastian.allard@scilifelab.se>
2023-11-22 14:53:22 +0100

    Start playback

--- quiz/clients/spotify_client.py
+++ quiz/clients/spotify_client.py
@@ -11,6 +11,7 @@ class SpotifyAPIClient:
         return {"Authorization": f"Bearer {access_token}"}
 
     def get_user_profile(self, user_id: str, access_token: str) -> UserProfile:
+        """Get public profile information about a Spotify user."""
         url = f"{self.base_url}/users/{user_id}"
         headers = self.get_headers(access_token)
         response = requests.get(url, headers)
@@ -19,13 +20,22 @@ class SpotifyAPIClient:
     def get_user_top_items(
         self, item_type: str, access_token: str, limit: int = 10, offset: int = 0
     ) -> TopItems:
+        """Get the current user's top artists or tracks based on calculated affinity."""
         url = f"{self.base_url}/me/top/{item_type}?limit={limit}&offset={offset}"
         headers = self.get_headers(access_token)
         response = requests.get(url, headers)
         return TopItems.model_validate(response.json())
 
     def get_followed_artists(self, access_token: str, limit: int = 10) -> FollowedArtists:
+        """Get the current user's followed artists."""
         url = f"{self.base_url}/me/following?type=artist&limit={limit}"
         headers = self.get_headers(access_token)
         response = requests.get(url, headers)
         return FollowedArtists.model_validate(response.json())
+
+    def start_playback(self, access_token: str, context_uri: str) -> None:
+        """Start a new context or resume current playback on the user's active device."""
+        url = f"{self.base_url}/me/player/play"
+        headers = self.get_headers(access_token)
+        data = {"context_uri": context_uri}
+        requests.put(url, headers=headers, json=data)
